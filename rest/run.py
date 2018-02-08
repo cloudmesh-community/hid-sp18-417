4d6d6fe59475e7b5cc7784eac2e5d5a2e3c9dbcd
rashray <35908747+rashray@users.noreply.github.com>
2018-02-07 23:50:20 -0500

    Update run.py

--- rest/run.py
+++ rest/run.py
@@ -1,31 +1,57 @@
-"""                                                          
-# A program to run the rest service to get sys info using eve
-# Date: 02/04/2018                                           
-# Author: Rashmi Ray                                         
-                                                             
-"""                                                          
-                                                             
-from eve import Eve                                          
-                                                             
-import platform                                              
-                                                             
-app = Eve()                                                  
-                                                             
-#Network Name                                                
-@app.route('/mysys/sys-name')                                
-def sysName():                                               
-     name = platform.node()                                  
-     return name                                             
-                                                             
-@app.route('/mysys/os-name')                                 
-def osName():                                                
-        name = platform.system()                             
-        return name                                          
-                                                             
-@app.route('/mysys/processor-name')                          
-def processorName():                                         
-         name = platform.processor()                         
-         return name                                         
-                                                             
-if __name__ == '__main__':                                   
-        app.run()                                            
+from eve import Eve
+import platform
+import json
+import psutil
+from flask import jsonify
+
+app = Eve()
+
+#Network Name
+@app.route('/sys-name')
+def sysName():
+     name = platform.node()
+     return name
+
+@app.route('/os-name')
+def osName():
+        name = platform.system()
+        return name
+
+@app.route('/processor-name')
+def processorName():
+         name = platform.processor()
+         return name
+
+@app.route('/disk-info')
+def getDiskInfo():
+         diskData = jsonify(psutil.disk_usage('/'))
+         return diskData
+
+@app.route('/all-info')
+def getAllInfo():
+        all_info = {
+                "diskData": {
+                        "disk-total": psutil.disk_usage('/').total,
+                        "disk-used": psutil.disk_usage('/').used,
+                        "disk-free": psutil.disk_usage('/').free,
+                        "disk-percent": psutil.disk_usage('/').percent
+                },
+                "systemData": {
+                         "machine": platform.machine(),
+                         "name": platform.node(),
+                         "processor": platform.processor(),
+                         "OS": platform.system(),
+                         "version": platform.version()
+                }
+        }
+        json_disk = (all_info)
+        response = app.response_class(
+                 response=json.dumps(all_info,sort_keys=True,indent=4),
+                 status=200,
+                 mimetype='application/json'
+             )
+        return response
+
+
+if __name__ == '__main__':
+        app.run()
