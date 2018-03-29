9c2108e2aea3f931f05eede76575d40266d69820
rashray <35908747+rashray@users.noreply.github.com>
2018-03-28 22:55:51 -0400

    Update setup.sh

--- tutorial/dhcp/setup.sh
+++ tutorial/dhcp/setup.sh
@@ -2,11 +2,21 @@
 
 ip=$1 # should be of format: 192.168.1.100
 dns=$2 # should be of format: 192.168.1.1
+echo "Installing Dependencies ..."
+apt-get update
+apt-get install -qy dnsmasq clusterssh iptables-persistent
+
+echo "Setting Up Head Node ..."
 
 # Set the static ip
 
 sudo cat <<EOT >> /etc/dhcpcd.conf
 interface eth0
+static ip_address=$ip/24
+static routers=$dns
+static domain_name_servers=$dns
+
+interface wlan0
 static ip_address=$ip/24
 static routers=$dns
 static domain_name_servers=$dns
