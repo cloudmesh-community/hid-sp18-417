8e2c98b6c84c2977e4339d8d9146ff0dc8893603
rashray <35908747+rashray@users.noreply.github.com>
2018-04-09 16:59:33 -0900

    Update setup.sh

--- tutorial/dhcp/setup.sh
+++ tutorial/dhcp/setup.sh
@@ -2,3 +2,5 @@ sudo apt-get update
 sudo apt-get install git
 mkdir /~/cloudmesh
 git clone https://github.com/cloudmesh-community/hid-sp18-417.git
+cd *417/tutotial
+sh dhcp_setup.sh raspi01 10.0.1.100 10.0.1.1 208.67.222.222
