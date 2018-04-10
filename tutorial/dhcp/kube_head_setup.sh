4edc7a6e98ce731abde60f9af7d2bdfcc05c90ce
rashray <35908747+rashray@users.noreply.github.com>
2018-04-10 09:57:20 -0400

    Update kube_head_setup.sh

--- tutorial/dhcp/kube_head_setup.sh
+++ tutorial/dhcp/kube_head_setup.sh
@@ -3,7 +3,7 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
   echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
   sudo apt-get update -q && \
   sudo apt-get install -qy kubeadm
-sudo kubeadm init --config kubeadm_conf.yaml
+sudo kubeadm init --token-ttl=0 --apiserver-advertise-address=<internal master ip>
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
