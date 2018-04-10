e4872aed470b17cf40aee939dbeceb2891150a4c
rashray <35908747+rashray@users.noreply.github.com>
2018-04-09 18:04:09 -0900

    Update kube_head_setup.sh

--- tutorial/dhcp/kube_head_setup.sh
+++ tutorial/dhcp/kube_head_setup.sh
@@ -3,5 +3,4 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
   echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
   sudo apt-get update -q && \
   sudo apt-get install -qy kubeadm
-curl -sSL https://rawgit.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml | sed "s/amd64/arm/g" | kubectl create -f -
-sudo kubeadm init --token-ttl=0 --apiserver-advertise-address=10.0.1.100 --pod-network-cidr=10.244.0.0/16
+sudo kubeadm init --config kubeadm_conf.yaml
