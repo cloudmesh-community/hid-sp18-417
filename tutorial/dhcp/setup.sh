09dded7dd973b4d34c792f17f9e907f1562b4c0a
rashray <35908747+rashray@users.noreply.github.com>
2018-04-09 15:53:27 -0900

    Update setup.sh

--- tutorial/dhcp/setup.sh
+++ tutorial/dhcp/setup.sh
@@ -1,24 +1,4 @@
-#!/bin/sh
-
-ip=$1 # should be of format: 192.168.1.100
-dns=$2 # should be of format: 192.168.1.1
-hostname=$3
-echo "Installing Dependencies ..."
-apt-get update
-apt-get install -qy dnsmasq clusterssh iptables-persistent
-
-echo "Setting Up Head Node ..."
-
-# Set the static ip
-
-sudo cat <<EOT >> /etc/dhcpcd.conf
-interface eth0
-static ip_address=$ip/24
-static routers=$dns
-static domain_name_servers=$dns
-
-interface wlan0
-static ip_address=$ip/24
-static routers=$dns
-static domain_name_servers=$dns
-EOT
+sudo apt-get update
+sudo apt-get install git
+mkdir /~/cloudmesh
+git clone https://github.com/cloudmesh-community/hid-sp18-417.git
